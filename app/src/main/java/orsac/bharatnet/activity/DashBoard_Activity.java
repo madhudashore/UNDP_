c63b1877c4463136008bb6b36a0c6bb6deb27a32
madhudashore <dashore.mad@gmail.com>
2018-09-27 18:26:28 +0530

    version 1.0

---
+++ app/src/main/java/orsac/bharatnet/activity/DashBoard_Activity.java
@@ -0,0 +1,11 @@
+package orsac.bharatnet.activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import orsac.bharatnet.panchayatDept.R;
+public class DashBoard_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board_);
+    }
+}
